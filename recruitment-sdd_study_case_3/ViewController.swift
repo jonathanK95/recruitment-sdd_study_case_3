74d5e0c0a7f735d3884e49c2e8d8d5f9e3b64b73
Jonathan Kristian <jonathankristian1995@gmail.com>
2024-04-03 10:20:09 +0700

    Initial Commit

---
+++ recruitment-sdd_study_case_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recruitment-sdd_study_case_3
+//
+//  Created by Jonathan Kristian on 03/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
